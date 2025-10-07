260e7ffbcd51299db5e90af5cfb9a7da18fdde5d
Riptir3 <thbence0116@gmail.com>
2025-10-07 13:14:55 +0200

    Designing main form.

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -28,33 +28,128 @@
         /// </summary>
         private void InitializeComponent()
         {
-            panel1 = new Panel();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            HeaderPanel = new Panel();
+            label1 = new Label();
+            exitLabel = new Label();
+            minLabel = new Label();
+            flowLayoutPanel1 = new FlowLayoutPanel();
+            homeBtn = new Button();
+            authorsBtn = new Button();
+            booksBtn = new Button();
+            statBtn = new Button();
+            HeaderPanel.SuspendLayout();
+            flowLayoutPanel1.SuspendLayout();
             SuspendLayout();
             // 
-            // panel1
+            // HeaderPanel
             // 
-            panel1.BackColor = SystemColors.ActiveCaptionText;
-            panel1.Dock = DockStyle.Top;
-            panel1.Location = new Point(0, 0);
-            panel1.Name = "panel1";
-            panel1.Size = new Size(828, 61);
-            panel1.TabIndex = 0;
+            HeaderPanel.BackColor = Color.FromArgb(35, 44, 65);
+            HeaderPanel.BorderStyle = BorderStyle.FixedSingle;
+            HeaderPanel.Controls.Add(label1);
+            HeaderPanel.Controls.Add(exitLabel);
+            HeaderPanel.Controls.Add(minLabel);
+            resources.ApplyResources(HeaderPanel, "HeaderPanel");
+            HeaderPanel.Name = "HeaderPanel";
+            // 
+            // label1
+            // 
+            resources.ApplyResources(label1, "label1");
+            label1.ForeColor = SystemColors.ButtonFace;
+            label1.Name = "label1";
+            // 
+            // exitLabel
+            // 
+            resources.ApplyResources(exitLabel, "exitLabel");
+            exitLabel.Cursor = Cursors.Hand;
+            exitLabel.ForeColor = SystemColors.ControlLightLight;
+            exitLabel.Name = "exitLabel";
+            exitLabel.Click += exitLabel_Click;
+            exitLabel.MouseLeave += exitLabel_MouseLeave;
+            exitLabel.MouseHover += exitLabel_MouseHover;
+            // 
+            // minLabel
+            // 
+            resources.ApplyResources(minLabel, "minLabel");
+            minLabel.Cursor = Cursors.Hand;
+            minLabel.ForeColor = SystemColors.ControlLightLight;
+            minLabel.Name = "minLabel";
+            minLabel.Click += minLabel_Click;
+            minLabel.MouseLeave += minLabel_MouseLeave;
+            minLabel.MouseHover += minLabel_MouseHover;
+            // 
+            // flowLayoutPanel1
+            // 
+            flowLayoutPanel1.BackColor = Color.FromArgb(35, 44, 65);
+            flowLayoutPanel1.Controls.Add(homeBtn);
+            flowLayoutPanel1.Controls.Add(authorsBtn);
+            flowLayoutPanel1.Controls.Add(booksBtn);
+            flowLayoutPanel1.Controls.Add(statBtn);
+            resources.ApplyResources(flowLayoutPanel1, "flowLayoutPanel1");
+            flowLayoutPanel1.Name = "flowLayoutPanel1";
+            // 
+            // homeBtn
+            // 
+            homeBtn.Cursor = Cursors.Hand;
+            homeBtn.FlatAppearance.BorderSize = 0;
+            resources.ApplyResources(homeBtn, "homeBtn");
+            homeBtn.ForeColor = SystemColors.ButtonHighlight;
+            homeBtn.Name = "homeBtn";
+            homeBtn.UseVisualStyleBackColor = true;
+            // 
+            // authorsBtn
+            // 
+            authorsBtn.Cursor = Cursors.Hand;
+            authorsBtn.FlatAppearance.BorderSize = 0;
+            resources.ApplyResources(authorsBtn, "authorsBtn");
+            authorsBtn.ForeColor = SystemColors.ButtonHighlight;
+            authorsBtn.Name = "authorsBtn";
+            authorsBtn.UseVisualStyleBackColor = true;
+            // 
+            // booksBtn
+            // 
+            booksBtn.Cursor = Cursors.Hand;
+            booksBtn.FlatAppearance.BorderSize = 0;
+            resources.ApplyResources(booksBtn, "booksBtn");
+            booksBtn.ForeColor = SystemColors.ButtonHighlight;
+            booksBtn.Name = "booksBtn";
+            booksBtn.UseVisualStyleBackColor = true;
+            // 
+            // statBtn
+            // 
+            statBtn.Cursor = Cursors.Hand;
+            statBtn.FlatAppearance.BorderSize = 0;
+            resources.ApplyResources(statBtn, "statBtn");
+            statBtn.ForeColor = SystemColors.ButtonHighlight;
+            statBtn.Name = "statBtn";
+            statBtn.UseVisualStyleBackColor = true;
             // 
             // Form1
             // 
-            AccessibleName = "";
-            AutoScaleDimensions = new SizeF(7F, 15F);
+            resources.ApplyResources(this, "$this");
             AutoScaleMode = AutoScaleMode.Font;
-            BackColor = SystemColors.ActiveCaption;
-            ClientSize = new Size(828, 916);
-            Controls.Add(panel1);
+            BackColor = Color.Black;
+            Controls.Add(flowLayoutPanel1);
+            Controls.Add(HeaderPanel);
             FormBorderStyle = FormBorderStyle.None;
             Name = "Form1";
+            Opacity = 0.75D;
+            HeaderPanel.ResumeLayout(false);
+            HeaderPanel.PerformLayout();
+            flowLayoutPanel1.ResumeLayout(false);
             ResumeLayout(false);
         }
 
         #endregion
 
-        private Panel panel1;
+        private Panel HeaderPanel;
+        private FlowLayoutPanel flowLayoutPanel1;
+        private Button homeBtn;
+        private Button authorsBtn;
+        private Button booksBtn;
+        private Button statBtn;
+        private Label minLabel;
+        private Label exitLabel;
+        private Label label1;
     }
 }
--- Form1.cs
+++ Form1.cs
@@ -4,12 +4,44 @@ namespace Library
 {
     public partial class Form1 : Form
     {
-        private readonly IUnitOfWork _unitOfWork;   
+        private readonly IUnitOfWork _unitOfWork;
         public Form1(IUnitOfWork unitOfWork)
         {
             InitializeComponent();
             _unitOfWork = unitOfWork;
         }
 
+        private void minLabel_Click(object sender, EventArgs e)
+        {
+            this.WindowState = FormWindowState.Minimized;
+        }
+
+        private void exitLabel_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+
+        private void exitLabel_MouseHover(object sender, EventArgs e)
+        {
+            exitLabel.ForeColor = Color.Red;
+            exitLabel.BackColor = Color.FromArgb(40, 50, 70);
+        }
+
+        private void exitLabel_MouseLeave(object sender, EventArgs e)
+        {
+            exitLabel.ForeColor = Color.White;
+            exitLabel.BackColor = Color.FromArgb(35, 44, 65);
+        }
+
+        private void minLabel_MouseHover(object sender, EventArgs e)
+        {
+            minLabel.BackColor = Color.FromArgb(40, 50, 70);
+        }
+
+        private void minLabel_MouseLeave(object sender, EventArgs e)
+        {
+            minLabel.BackColor = Color.FromArgb(35, 44, 65);
+            minLabel.ForeColor = Color.White;
+        }
     }
 }
