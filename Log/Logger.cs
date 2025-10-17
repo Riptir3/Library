ce046bd53ed99e3ddb5b62abd9ddbfc2bb899605
Riptir3 <thbence0116@gmail.com>
2025-10-17 10:03:11 +0200

    Add Logger.

---
+++ Log/Logger.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Library.Log
+{
+    public enum LogLevel
+    {
+        Info,
+        Warning,
+        Error
+    }
+
+    public class AdvancedLoggerSync
+    {
+        private readonly string _logDirectory;
+        private readonly object _lock = new object();
+
+        public AdvancedLoggerSync(string logDirectory)
+        {
+            _logDirectory = logDirectory;
+
+            if (!Directory.Exists(_logDirectory))
+                Directory.CreateDirectory(_logDirectory);
+        }
+
+        public void Log(string message, LogLevel level = LogLevel.Info)
+        {
+            string logEntry = $"[{DateTime.Now:yyyy-MM-dd HH:mm:ss}] [{level}] {message}";
+            string filePath = GetLogFilePath();
+
+            lock (_lock)
+            {
+                File.AppendAllText(filePath, logEntry + Environment.NewLine);
+            }
+        }
+
+        private string GetLogFilePath()
+        {
+            string fileName = $"log_{DateTime.Now:yyyyMMdd}.txt";
+            return Path.Combine(_logDirectory, fileName);
+        }
+    }
+
+}
